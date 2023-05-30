348bb6b2b8a14002f863a8cacb8d2eafe77d1d3a
JH <vvaa277@gmail.com>
2023-05-30 18:50:29 +0900

    feat: rolling 배너 store 구현

---
+++ src/store/rollingStore.ts
@@ -0,0 +1,62 @@
+import { createDispatcher } from "../dispatcher/dispatcher";
+
+const dispatcher = createDispatcher();
+
+const rollingState = {
+  leftInterval: true,
+  rightInterval: true,
+  leftIndex: 0,
+  rightIndex: 0,
+};
+
+const setIntervalListener = dispatcher.register(function (action) {
+  switch (action.type) {
+    case "START_LEFT_INTERVAL":
+      rollingState.leftInterval = true;
+      break;
+    case "STOP_LEFT_INTERVAL":
+      rollingState.leftInterval = false;
+      break;
+    case "START_RIGHT_INTERVAL":
+      rollingState.rightInterval = true;
+      break;
+    case "STOP_RIGHT_INTERVAL":
+      rollingState.rightInterval = false;
+      break;
+    case "LEFT_INDEX":
+      changeLeftIndex();
+      break;
+    case "RIGHT_INDEX":
+      changeRightIndex();
+      break;
+    default:
+      break;
+  }
+});
+
+export function getLeftIntervalState(): boolean {
+  return rollingState.leftInterval;
+}
+export function getRightIntervalState(): boolean {
+  return rollingState.rightInterval;
+}
+export function getLeftIndexState(): number {
+  return rollingState.leftIndex;
+}
+export function getRightIndexState(): number {
+  return rollingState.rightIndex;
+}
+function changeLeftIndex() {
+  rollingState.leftIndex += 1;
+  if (rollingState.leftIndex === 5) {
+    rollingState.leftIndex = 0;
+  }
+}
+function changeRightIndex() {
+  rollingState.rightIndex += 1;
+  if (rollingState.rightIndex === 5) {
+    rollingState.rightIndex = 0;
+  }
+}
+
+export { dispatcher };
