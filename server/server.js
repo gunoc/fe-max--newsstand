5598c366275b53e4feaaabf88456b6cb11c8fe0c
JH <vvaa277@gmail.com>
2023-06-05 14:43:07 +0900

    chore: 서버파일 수정

--- server/server.js
+++ server/server.js
@@ -18,6 +18,16 @@ app.get("/rolling", (req, res) => {
   });
 });
 
+app.get("/grid", (req, res) => {
+  fs.readFile("server/data/grid.json", "utf-8", (err, data) => {
+    if (err) {
+      res.status(500).send("Error reading JSON file");
+    } else {
+      res.send(JSON.parse(data));
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`Express server is running at http://localhost:${port}`);
 });
