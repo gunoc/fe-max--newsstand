7cf97bd7d62c729074f3a7f9b4efb03a275fce9a
JH <vvaa277@gmail.com>
2023-06-05 17:30:24 +0900

    feat: actions 추가

--- src/actions/mediaActions.ts
+++ src/actions/mediaActions.ts
@@ -1,38 +1,44 @@
-import { dispatch } from "../store/store";
+import { store } from "../store/store";
 
 export const fetchActionCreator = {
   async fetchGridData() {
     const response = await fetch("http://localhost:3000/grid");
     const data = await response.json();
 
-    dispatch({ type: "LOAD_GRID_DATA", payload: data });
+    store.dispatch({ type: "LOAD_GRID_DATA", payload: data });
   },
 };
 
 export const displayActionCreator = {
   clickAllPressView() {
-    dispatch({ type: "CLICK_ALL_PRESS_VIEW" });
+    store.dispatch({ type: "CLICK_ALL_PRESS_VIEW" });
   },
+
   clickSubscriptionView() {
-    dispatch({ type: "CLICK_SUBSCRIPTION_VIEW" });
+    store.dispatch({ type: "CLICK_SUBSCRIPTION_VIEW" });
   },
+
   clickGridView() {
-    dispatch({ type: "CLICK_GRID_VIEW" });
+    store.dispatch({ type: "CLICK_GRID_VIEW" });
   },
+
   clickListView() {
-    dispatch({ type: "CLICK_LIST_VIEW" });
+    store.dispatch({ type: "CLICK_LIST_VIEW" });
   },
+
   clickPrevButton() {
-    dispatch({ type: "CLICK_PREV_BUTTON" });
+    store.dispatch({ type: "CLICK_PREV_BUTTON" });
   },
+
   clickNextButton() {
-    dispatch({ type: "CLICK_NEXT_BUTTON" });
+    store.dispatch({ type: "CLICK_NEXT_BUTTON" });
   },
 
   clickAddSubscription() {
-    dispatch({ type: "CLICK_ADD_SUBSCRIPTION" });
+    store.dispatch({ type: "CLICK_ADD_SUBSCRIPTION" });
   },
+
   clickUnsubscribe() {
-    dispatch({ type: "CLICK_UNSUBSCRIBE" });
+    store.dispatch({ type: "CLICK_UNSUBSCRIBE" });
   },
 };
