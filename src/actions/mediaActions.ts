b252056d0fb482cebe0b613a20a769b827fc51b7
JH <vvaa277@gmail.com>
2023-06-11 14:49:38 +0900

    feat: 액션 타입 상수화

--- src/actions/mediaActions.ts
+++ src/actions/mediaActions.ts
@@ -1,3 +1,4 @@
+import { ActionTypes } from "../constants/constants";
 import { store } from "../store/store";
 import { shuffleArray } from "../utils/shuffleArray";
 
@@ -6,48 +7,48 @@ export const fetchActionCreator = {
     const response = await fetch("http://localhost:3000/grid");
     const data = await response.json();
     const shuffledData = shuffleArray(data);
-    store.dispatch({ type: "LOAD_GRID_DATA", payload: shuffledData });
+    store.dispatch({ type: ActionTypes.LOAD_GRID_DATA, payload: shuffledData });
   },
 };
 
 export const displayActionCreator = {
   clickAllPressView() {
-    store.dispatch({ type: "CLICK_ALL_PRESS_VIEW" });
+    store.dispatch({ type: ActionTypes.CLICK_ALL_PRESS_VIEW });
   },
 
   clickSubscriptionView() {
-    store.dispatch({ type: "CLICK_SUBSCRIPTION_VIEW" });
+    store.dispatch({ type: ActionTypes.CLICK_SUBSCRIPTION_VIEW });
   },
 
   clickGridView() {
-    store.dispatch({ type: "CLICK_GRID_VIEW" });
+    store.dispatch({ type: ActionTypes.CLICK_GRID_VIEW });
   },
 
   clickListView() {
-    store.dispatch({ type: "CLICK_LIST_VIEW" });
+    store.dispatch({ type: ActionTypes.CLICK_LIST_VIEW });
   },
 
   clickPrevButton() {
-    store.dispatch({ type: "CLICK_PREV_BUTTON" });
+    store.dispatch({ type: ActionTypes.CLICK_PREV_BUTTON });
   },
 
   clickNextButton() {
-    store.dispatch({ type: "CLICK_NEXT_BUTTON" });
+    store.dispatch({ type: ActionTypes.CLICK_NEXT_BUTTON });
   },
 
-  clickAddSubscription(payload) {
-    store.dispatch({ type: "CLICK_ADD_SUBSCRIPTION", payload });
+  clickAddSubscription(payload: any) {
+    store.dispatch({ type: ActionTypes.CLICK_ADD_SUBSCRIPTION, payload });
   },
 
-  clickRemoveSubscribe(payload) {
-    store.dispatch({ type: "CLICK_REMOVE_SUBSCRIBE", payload });
+  clickRemoveSubscribe(payload: any) {
+    store.dispatch({ type: ActionTypes.CLICK_REMOVE_SUBSCRIBE, payload });
   },
 
-  displayAlert(payload) {
-    store.dispatch({ type: "DISPLAY_ALERT", payload });
+  displayAlert(payload: any) {
+    store.dispatch({ type: ActionTypes.DISPLAY_ALERT, payload });
   },
 
   hideAlert() {
-    store.dispatch({ type: "HIDE_ALERT" });
+    store.dispatch({ type: ActionTypes.HIDE_ALERT });
   },
 };
