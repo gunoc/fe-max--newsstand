62538a817e43b4905d41e3181bd6e56b52d845f7
JH <vvaa277@gmail.com>
2023-06-14 02:41:23 +0900

    feat: 상수 추가

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,6 @@
 export enum ActionTypes {
   LOAD_GRID_DATA = "LOAD_GRID_DATA",
+  LOAD_LIST_DATA = "LOAD_LIST_DATA",
   CLICK_ALL_PRESS_VIEW = "CLICK_ALL_PRESS_VIEW",
   CLICK_GRID_VIEW = "CLICK_GRID_VIEW",
   CLICK_SUBSCRIPTION_VIEW = "CLICK_SUBSCRIPTION_VIEW",
@@ -14,4 +15,11 @@ export enum ActionTypes {
 
 export enum Numbers {
   ITEMS_PER_PAGE = 24,
+  MAX_GRID_PAGE = 3,
+}
+
+export enum FetchUrls {
+  BASE_URL = "http://localhost:3000",
+  GRID_FETCH_URL = "grid",
+  LIST_FETCH_URL = "list",
 }
